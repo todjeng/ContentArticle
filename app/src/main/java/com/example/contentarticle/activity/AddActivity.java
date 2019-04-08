a9aa24c9d20121ef6539897d6f26a3a6d3cd2869
todjeng <ariharyanto7@gmail.com>
2019-04-08 17:07:51 +0700

    perbaikan method save dan Create item content

--- app/src/main/java/com/example/contentarticle/activity/AddActivity.java
+++ app/src/main/java/com/example/contentarticle/activity/AddActivity.java
@@ -63,7 +63,7 @@ public class AddActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        setContentView(R.layout.add);
 
 
         judul = (EditText) findViewById(R.id.judul);

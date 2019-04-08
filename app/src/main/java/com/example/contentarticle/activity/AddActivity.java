4bef2f60ed701c5045cf01cd7532e9a0dc926cb0
todjeng <ariharyanto7@gmail.com>
2019-04-08 14:05:38 +0700

    ini commit kedua saya

--- app/src/main/java/com/example/contentarticle/activity/AddActivity.java
+++ app/src/main/java/com/example/contentarticle/activity/AddActivity.java
@@ -1,39 +1,90 @@
 package com.example.contentarticle.activity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.contentarticle.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class AddActivity extends AppCompatActivity {
 
-    private Toolbar mtToolbar;
     private EditText judul, content, tanggal, phone;
     private Button save;
+    private ImageButton btnBack;
+
+    /*
+    *Inisiasi kalender
+     */
+    final Calendar myCalendar = Calendar.getInstance();
+
+    /*
+    *inisiasi listener ketika klik ok
+     */
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    /**
+     * Method untuk update format dari DatePickerDialog ke EditText
+     */
+    private void updateLabel(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        tanggal.setText(sdf.format(myCalendar.getTime()));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        mtToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mtToolbar);
 
         judul = (EditText) findViewById(R.id.judul);
+        content = (EditText) findViewById(R.id.content);
         tanggal = (EditText) findViewById(R.id.tanggal);
         phone = (EditText) findViewById(R.id.phone);
 
         save = (Button) findViewById(R.id.btnSave);
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
+
+        tanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddActivity.this, date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(AddActivity.this, "Oke", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddActivity.this,HomeActivity.class));
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/contentarticle/dao/ContentDao.java
+++ app/src/main/java/com/example/contentarticle/dao/ContentDao.java
@@ -1,9 +1,11 @@
 package com.example.contentarticle.dao;
 
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 
 import com.example.contentarticle.model.room.Content;
 
+@Dao
 public interface ContentDao {
 
     @Insert

3b8a385b9e29cb78a1b8c3796e8989d4edc0e6de
todjeng <ariharyanto7@gmail.com>
2019-04-09 13:18:41 +0700

    create intent detail dan call phone

--- app/src/main/java/com/example/contentarticle/activity/DetailContentActivity.java
+++ app/src/main/java/com/example/contentarticle/activity/DetailContentActivity.java
@@ -1,6 +1,11 @@
 package com.example.contentarticle.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +14,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.contentarticle.R;
+import com.example.contentarticle.model.room.Content;
 
 public class DetailContentActivity extends AppCompatActivity {
 
@@ -32,6 +38,40 @@ public class DetailContentActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(DetailContentActivity.this, HomeActivity.class));
                 finish();
+            }
+        });
+
+        /**
+         * Mengambil data content dari passing data (putExtra)
+         */
+        final Content content = (Content) getIntent().getSerializableExtra("detailContent");
+        contentDetail(content);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                    && checkSelfPermission(Manifest.permission.CALL_PHONE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        110);
+            }
+            return;
+        }
+    }
+
+    private void contentDetail(final Content content){
+        mUsername.setText(content.getJudul());
+        mContent.setText(content.getMyContent());
+        mGender.setText(content.getCategory());
+        mDate.setText(content.getTanggal());
+        mPhone.setText(content.getPhone());
+
+        mPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + content.getPhone()));
+                startActivity(callIntent);
             }
         });
     }
--- app/src/main/java/com/example/contentarticle/adapter/ContentAdapter.java
+++ app/src/main/java/com/example/contentarticle/adapter/ContentAdapter.java
@@ -1,6 +1,8 @@
 package com.example.contentarticle.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,6 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.contentarticle.R;
+import com.example.contentarticle.activity.DetailContentActivity;
 import com.example.contentarticle.model.room.Content;
 
 import java.util.List;
@@ -54,11 +57,18 @@ public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ContentV
             mTanggal = itemView.findViewById(R.id.tanggal);
             mBtnEdit = itemView.findViewById(R.id.btnEdit);
             mBtnDelete = itemView.findViewById(R.id.btnDelete);
+
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
+            Content content = contentList.get(getAdapterPosition());
 
+            Intent intent = new Intent(mContext, DetailContentActivity.class);
+            intent.putExtra("detailContent", content);
+            mContext.startActivity(intent);
+            ((Activity) mContext).finish();
         }
     }
 }

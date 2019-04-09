afa792faec2d9fd41fe4af5ee56267a823142e3e
todjeng <ariharyanto7@gmail.com>
2019-04-09 15:22:46 +0700

    create edit data

--- app/src/main/java/com/example/contentarticle/activity/UpdateActivity.java
+++ app/src/main/java/com/example/contentarticle/activity/UpdateActivity.java
@@ -1,17 +1,26 @@
 package com.example.contentarticle.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.contentarticle.R;
+import com.example.contentarticle.helper.DatabaseClient;
 import com.example.contentarticle.model.room.Content;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class UpdateActivity extends AppCompatActivity {
 
@@ -22,6 +31,34 @@ public class UpdateActivity extends AppCompatActivity {
     Button mBtnSave;
 
     String status;
+
+    /*
+     *Inisiasi kalender
+     */
+    final Calendar myCalendar = Calendar.getInstance();
+
+    /*
+     *inisiasi listener ketika klik ok
+     */
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    /**
+     * Method untuk update format dari DatePickerDialog ke EditText
+     */
+    private void updateLabel(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        mTanggal.setText(sdf.format(myCalendar.getTime()));
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +76,19 @@ public class UpdateActivity extends AppCompatActivity {
         mBtnSave = (Button) findViewById(R.id.btnSaveUpdt);
         status = "";
 
+
+        /**
+         * event ketika kolom tanggal di klik
+         */
+        mTanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(UpdateActivity.this, date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
         mBtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,8 +99,37 @@ public class UpdateActivity extends AppCompatActivity {
 
         final Content content = (Content) getIntent().getSerializableExtra("editContent");
         editContent(content);
+
+        /**
+         * Untuk event radio button
+         */
+        mRadio_status.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio_maleUpdt:
+                        status = "Male";
+                        break;
+                    case R.id.radio_femaleUpdt:
+                        status = "Female";
+                        break;
+                }
+            }
+        });
+
+        mBtnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updatedProses(content);
+            }
+        });
     }
 
+
+    /**
+     * Tampil Content Yang Akan Di Edit
+     * @param content
+     */
     private void editContent(final Content content){
         mJudul.setText(content.getJudul());
         mContent.setText(content.getMyContent());
@@ -67,4 +146,39 @@ public class UpdateActivity extends AppCompatActivity {
         }
 
     }
+
+    private void updatedProses(final Content content){
+        final String judul = mJudul.getText().toString();
+        final String txtContent = mContent.getText().toString();
+        final String tanggal = mTanggal.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String category = status;
+
+            class Updated extends AsyncTask<Void,Void,Void>{
+
+                @Override
+                protected Void doInBackground(Void... voids) {
+                    content.setJudul(judul);
+                    content.setMyContent(txtContent);
+                    content.setTanggal(tanggal);
+                    content.setPhone(phone);
+                    content.setCategory(category);
+
+                    DatabaseClient.getInstance(getApplicationContext()).getAppDatabase()
+                            .contentDao().update(content);
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    Toast.makeText(UpdateActivity.this, "Data Berhasi Di Updated", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(UpdateActivity.this, HomeActivity.class));
+                    finish();
+                }
+            }
+
+            Updated updated = new Updated();
+            updated.execute();
+     }
 }
--- app/src/main/java/com/example/contentarticle/dao/ContentDao.java
+++ app/src/main/java/com/example/contentarticle/dao/ContentDao.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import com.example.contentarticle.model.room.Content;
 
@@ -20,5 +21,9 @@ public interface ContentDao {
 
     @Delete
     void delete(Content content);
+
+    @Update
+    void update(Content content);
+
 
 }
